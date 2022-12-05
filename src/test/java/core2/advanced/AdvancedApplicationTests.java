551a0bfbfb9768eb5cedfac4b86590f27454255f
miyeon396 <miyeon396@gmail.com>
2022-12-05 22:43:48 +0900

    init

---
+++ src/test/java/core2/advanced/AdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package core2.advanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
